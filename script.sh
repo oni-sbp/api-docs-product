b4c7cb909ccfa0fa49a9ead33b0cbfe4bdb53ba9
oni-sbp <58467574+oni-sbp@users.noreply.github.com>
2020-01-14 11:24:30 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,18 +1,18 @@
 # Install MongoDB
 # https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-sudo apt-get install gnupg
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-echo "mongodb-org hold" | sudo dpkg --set-selections
-echo "mongodb-org-server hold" | sudo dpkg --set-selections
-echo "mongodb-org-shell hold" | sudo dpkg --set-selections
-echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
-echo "mongodb-org-tools hold" | sudo dpkg --set-selections
-sudo service mongod start
-sudo service mongod status
+# wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+# sudo apt-get install gnupg
+# wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+# echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+# sudo apt-get update
+# sudo apt-get install -y mongodb-org
+# echo "mongodb-org hold" | sudo dpkg --set-selections
+# echo "mongodb-org-server hold" | sudo dpkg --set-selections
+# echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+# echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+# echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+# sudo service mongod start
+# sudo service mongod status
 
 
 # Install ruby
@@ -27,7 +27,11 @@ sudo service mongod status
 # apt install python-pip
 
 cd api-docs-product
-mkdir resources/Temp-Files
+# mkdir resources/Temp-Files
+
+git stash save
+git stash drop 
+git pull
 
 chmod +x index.js
 npm install
@@ -44,8 +48,8 @@ npm install
 # cd ..
 # cd ..
 
-cp service/codegen.service /etc/systemd/system/codegen.service
+# cp service/codegen.service /etc/systemd/system/codegen.service
 
 
-sudo systemctl enable codegen.service
-systemctl start codegen.service
+# sudo systemctl enable codegen.service
+systemctl restart codegen.service
